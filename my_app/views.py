3a56fae05c2ddf33292f5baf24ba5ccae6837be0
TropicolCoder <jacobsbusayo@gmail.com>
2021-01-15 18:36:58 +0100

    Third Commit

--- my_app/views.py
+++ my_app/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render
+import requests
+from bs4 import BeautifulSoup
 
 
 # Create your views here.
 def home(request):
-    return render(request, template_name='base.html')
+    return render(request, template_name='my_app/index.html')
+
+
+def new_search(request):
+    search = request.POST.get('search')
+    stuff_for_frontend = {
+        'search': search,
+        'title': 'Xanderslist',
+    }
+    return render(request, 'my_app/new_search.html', stuff_for_frontend)
+
+
+
